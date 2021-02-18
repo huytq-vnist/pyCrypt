dbd0326db6cb7979ddf2cbee24ccfe6f84a491f1
Tran Quang Huy 20173180 <huy.tq173180@sis.hust.edu.vn>
2021-02-18 15:52:08 +0700

    ready

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'ƯICKED',                   # Type in your name
+  author = 'WICKED',                   # Type in your name
   author_email = 'huytq.vnist@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/huytq-vnist/pyCrypt/tree/master/pyCrypt',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/huytq-vnist/pyCrypt/archive/v_01.tar.gz',    # I explain this later on
@@ -14,7 +14,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
